85598c8b80d665f1302d14a61d47769f92f22a0b
SandypKnda <sandeepmsbi631@gmail.com>
2025-05-26 21:26:52 -0700

    Create FastAPIEndpoint.py

---
+++ FastAPIEndpoint.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from query import fetch_recent_jobs
+from filter import filter_jobs
+from alert import send_email_alert
+
+app = FastAPI()
+
+@app.post("/alert")
+def run_alerting():
+    jobs = fetch_recent_jobs()
+    good_jobs = filter_jobs(jobs)
+    send_email_alert(good_jobs)
+    return {"filtered": len(good_jobs)}
