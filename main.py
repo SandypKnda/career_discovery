5aa49c7563ef773eacb747f602797c0d544017a2
SandypKnda <sandeepmsbi631@gmail.com>
2025-06-13 14:23:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,11 @@ from alert import send_email_alert
 
 app = FastAPI()
 
-# Remove this code from 9-13 after testing
 
-@app.get("/company-list")
-def get_company_list(role: str = "data engineer"):
-    # TODO: Replace with your dynamic discovery logic
-    companies = ["Airbnb", "Stripe", "Netflix", "Snowflake", "Databricks"]
-    return companies
+@app.post("/alert")
+def run_alerting():
+    jobs = fetch_recent_jobs()
+    good_jobs = filter_jobs(jobs)
+    send_email_alert(good_jobs)
+    return {"filtered": len(good_jobs)}
 
-
-#@app.post("/alert")
-#def run_alerting():
-#    jobs = fetch_recent_jobs()
-#    good_jobs = filter_jobs(jobs)
-#    send_email_alert(good_jobs)
-#    return {"filtered": len(good_jobs)}
-#'''
